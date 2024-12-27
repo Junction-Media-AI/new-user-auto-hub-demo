ad2a9bb526bce8cd5dc58c141de8982382d5ad28
Rigelle Cruz <rigellecruz@gmail.com>
2024-12-27 16:00:02 +1300

    Create navigation bar

--- client/components/UI/Navigation.tsx
+++ client/components/UI/Navigation.tsx
@@ -1,5 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+
 const Navigation = () => {
-	return <div>Navigation</div>;
+	const navigate = useNavigate();
+	function goTo(link: string) {
+		navigate(link);
+	}
+
+	return (
+		<nav className="nav">
+			<button onClick={() => goTo('/dashboard')} className="nav-button">
+				Dashboard
+			</button>
+			<button onClick={() => goTo('/automation-hub')} className="nav-button">
+				Automation Hub
+			</button>
+			<button onClick={() => goTo('/account')} className="nav-button">
+				Account
+			</button>
+		</nav>
+	);
 };
 
 export default Navigation;
